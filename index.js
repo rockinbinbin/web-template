26053af5cc1c063390806ed14ef32f3372c27521
Robin <robimeht@umich.edu>
2017-03-09 13:18:21 -0800

    heroku tutorial works!

--- index.js
+++ index.js
@@ -27,9 +27,13 @@ app.get('/times', function(request, response) {
 });
 
 var pg = require('pg');
+var connectionString = "postgres://widpkkeosrpqla:d0878b74676286258833e9afedb8fbd024aa30418f8cdd85a6be3e2f833202e8@ec2-107-22-223-6.compute-1.amazonaws.com:5432/d9n4l2gm0uf8d1?ssl=true"
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(connectionString, function(err, client, done) {
+  	if (err) {
+  		console.error(err); response.send("Error " + err); 
+  	}
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
