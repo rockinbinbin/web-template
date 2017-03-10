43613fd9cf93435b82b6d022d4636bd987af8796
Robin <robimeht@umich.edu>
2017-03-09 16:16:13 -0800

    edited table in migration file

--- migrations/1489099857110.js
+++ migrations/1489099857110.js
@@ -3,14 +3,25 @@
 var pg = require('pg');
 var connectionString = "postgres://widpkkeosrpqla:d0878b74676286258833e9afedb8fbd024aa30418f8cdd85a6be3e2f833202e8@ec2-107-22-223-6.compute-1.amazonaws.com:5432/d9n4l2gm0uf8d1?ssl=true"
 
+var rollback = function(client, done) {
+  client.query('ROLLBACK', function(err) {
+    //if there was a problem rolling back the query
+    //something is seriously messed up.  Return the error
+    //to the done function to close & remove this client from
+    //the pool.  If you leave a client in the pool with an unaborted
+    //transaction weird, hard to diagnose problems might happen.
+    return done(err);
+  });
+};
+
 exports.up = function(next) {
-	console.log("exports up");
+  console.log("exports up");
   pg.connect(connectionString, function(err, client, done) {
   	console.log("connection up");
   	if (err) {
   		console.error(err); response.send("Error " + err); 
   	}
-    client.query('create table test_table (id integer, name text)', function(err, result) {
+    client.query('create table if not exists test_table (id integer, name text, formatted text, raw text)', function(err, result) {
       done();
       next();
       if (err)
@@ -24,7 +35,7 @@ exports.down = function(next) {
   	if (err) {
   		console.error(err); response.send("Error " + err); 
   	}
-    client.query('DROP TABLE test_table', function(err, result) {
+    client.query('DROP TABLE if exists test_table', function(err, result) {
       done();
       next();
       if (err)
